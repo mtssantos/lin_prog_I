cdaf940fb9e3af655041dac82b2027270f884cf3
Mateus Santos <mtscostadossantos@gmail.com>
2023-03-10 00:44:42 -0300

    First Commit

---
+++ src/entities/EquipeTI.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class EquipeTI {
+        private String nome;
+        private String email;
+        private Double salario;
+
+        public EquipeTI(String nome, String email, double salario){
+            this.nome = nome;
+            this.email = email;
+            this.salario = salario;
+        }
+
+        public void setNome(String nome){
+            this.nome = nome;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setSalario(double salario) {
+            this.salario = salario;
+        }
+
+        public Double getSalario() {
+            return salario;
+        }
+
+        public void Aumento(double valor){
+            this.salario += valor;
+        }
+}
